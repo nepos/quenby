f2d92af8de04cf8c21efeb1dc49abb0a244ec714
Daniel Mack <daniel@zonque.org>
2016-12-01 13:59:04 +0100

    make the main window full screen with no window decoration

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+
+    w.setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+    w.setWindowState(Qt::WindowFullScreen);
+    w.showFullScreen();
 
     return a.exec();
 }
